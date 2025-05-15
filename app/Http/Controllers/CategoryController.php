143b2a637f6bc568d9ce83e01e32a8a68b55fcde
Well Gatse <wellgatse@gmail.com>
2025-05-15 19:18:30 +0200

    feat(category): Ajout du modèle, la migration et du controller

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make(
+            $request->all(), [
+                'title'=>'required|string|max:255|unique:categories',
+                'description'=>'required|max:255',
+            ]
+            );
+
+            if($validator->fails())
+            {
+                return response()->json([
+                    'success'=>false,
+                    'message'=>$validator->errors()
+                ]);
+            }
+
+            $category = Category::create($request->all());
+
+
+            return response()->json([
+                'success'=>true,
+                'message'=>'insertion réussit',
+                'data'=>$category
+            ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}

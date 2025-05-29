d1cb44704533bc56ee43d3031d6fe6c3c3d7a090
Sodja1234 <franklinkabisayi@gmail.com>
2025-05-29 14:46:28 +0100

    Creation du Seeder pour les Events

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -3,9 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class Ticket extends Model
 {
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasApiTokens, HasFactory, Notifiable;
+    
      protected $fillable = [
         'event_id',
         'name',

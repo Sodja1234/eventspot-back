cfdd32bfa96b66e43ac4c50a8022307dfc6e34bf
Sodja1234 <franklinkabisayi@gmail.com>
2025-05-29 14:45:25 +0100

    Creation des Factory pour les events, les categories et les tickets

---
+++ database/factories/TicketFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Ticket>
+ */
+class TicketFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->unique()->word(),
+            'price' => fake()->numberBetween(10, 50),
+            'places' => fake()->numberBetween(100,500),
+            'description' => fake()->sentence(),
+            'reserved_places' => fake()->numberBetween(0, 20),
+        ];
+    }
+}

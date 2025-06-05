61066a795be841822209c55e356391391472ecfb
Mgreat01 <votre-email@example.com>
2025-06-05 16:06:03 +0200

    amelioration du controller d enregistrement d utilisateur et du fichier registerRequest

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -5,6 +5,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use App\Models\Organisateur;
 use Illuminate\Auth\Events\Registered;
@@ -13,6 +14,7 @@ use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
+use Illuminate\Support\Facades\Log;
 
 class RegisteredUserController extends Controller
 {
@@ -21,25 +23,20 @@ class RegisteredUserController extends Controller
      *
      * @throws \Illuminate\Validation\ValidationException
      */
-    public function store(Request $request): Response
-        {
-            $validated = $request->validate([
-                'fullname' => 'required|string',
-                'email' => 'required|email|unique:users',
-                'password' => 'required|min:6|confirmed',
-                'role' => 'required|in:public,organisateur',
-                'nom_organis' => 'required_if:role,organisateur',
-                'interets' => 'array|required_if:role,public',
-                'interets.*' => 'exists:interets,id',
-            ]);
-        
+    public function store(RegisterRequest $request): Response
+
+
+    {
+        try {
+            $validated = $request->validated();
+
             $user = User::create([
-                'fullname' => $validated['fullname'],
+                'name' => $validated['name'],
                 'email' => $validated['email'],
                 'password' => bcrypt($validated['password']),
                 'role' => $validated['role'],
             ]);
-        
+
             if ($user->role === 'organisateur') {
                 Organisateur::create([
                     'user_id' => $user->id,
@@ -48,9 +45,31 @@ class RegisteredUserController extends Controller
             } elseif ($user->role === 'public') {
                 $user->interets()->attach($validated['interets']);
             }
+
             $user->sendEmailVerificationNotification();
+
+
+
+
+            //
+
+            // Génération OTP
+
+
+
+
+
+
+            //
             return response()->noContent();
+
+        } catch (\Exception $e) {
+            Log::error('Erreur lors de l’inscription: ' . $e->getMessage());
+
+            return response()->noContent();
+        }
     }
+
 }
 
 

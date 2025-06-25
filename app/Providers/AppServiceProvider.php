e8360a88433bf666707e6508bb29198fbc6439d4
Mgreat01 <ephraimmonga5@gmail.com>
2025-06-25 13:35:44 +0200

    personnalistion du message de confirmation d email et ajout de l otp dans ce message

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,8 @@ use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 Use Illuminate\Support\Facades\URL;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -31,12 +33,25 @@ class AppServiceProvider extends ServiceProvider
 public function boot(): void
 {
     // Personnalisation de l'URL envoyée par mail
+    Schema::defaultStringLength(191);
     ResetPassword::createUrlUsing(function ($notifiable, string $token) {
         $frontendUrl = config('app.frontend_url', 'http://localhost:4200');
         $email = urlencode($notifiable->getEmailForPasswordReset());
 
         return "{$frontendUrl}/reset-password?token={$token}&email={$email}";
     });
-}
 
+     // Email personnalisé de vérification avec lien + OTP
+        VerifyEmail::toMailUsing(function ($notifiable, $url) {
+            $otp = $notifiable->emailOtp()->latest()->first()?->otp ?? '******';
+
+            return (new MailMessage)
+                ->subject('Vérification de votre adresse e-mail')
+                ->line('Bienvenue sur notre plateforme.')
+                ->line(" Votre code OTP est : **$otp**")
+                ->action('Vérifier mon email', $url)
+                ->line('Ce lien expirera dans 60 minutes.')
+                ->line('Si vous n’avez pas créé de compte, ignorez ce message.');
+        });
+    }
 }

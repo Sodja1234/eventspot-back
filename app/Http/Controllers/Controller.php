eaa7d32262a90d7c7f8c8d61e282f3da541f7af3
Sodja1234 <franklinkabisayi@gmail.com>
2025-07-10 14:50:44 +0100

    Documentation des Controller CategoryController, Controller et DashboardController

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,33 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="EventSpot API Documentation",
+ *      description="API Documentation for EventSpot, a platform to discover and manage events.",
+ *      @OA\Contact(
+ *          email="admin@eventspot.com"
+ *      ),
+ *      @OA\License(
+ *          name="Apache 2.0",
+ *          url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *      )
+ * )
+ *
+ * @OA\Server(
+ *      url=L5_SWAGGER_CONST_HOST,
+ *      description="EventSpot API Server"
+ * )
+ *
+ * @OA\SecurityScheme(
+ *     securityScheme="bearerAuth",
+ *     type="http",
+ *     scheme="bearer",
+ *     bearerFormat="JWT",
+ *     description="Enter token in format (Bearer <token>)"
+ * )
+ */
 abstract class Controller
 {
     //

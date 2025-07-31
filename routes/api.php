a88a146f0379e1ae2698cf266252a5072ecaa87f
Sodja1234 <franklinkabisayi@gmail.com>
2025-07-31 16:08:34 +0100

    creation de la route events/auth dans API Route

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,7 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function () {
     Route::prefix('events')->group(
         function () {
             Route::get('auth/{id}', [EventController::class, 'show']);
+            Route::get('auth', [EventController::class, 'index']);
             Route::post('{event_id}/favorite', [FavoriteController::class, '__invoke']);
             Route::get('{event_id}/favorite', [FavoriteController::class, 'show']);
             Route::post('{event_id}/subscribe', [SubscribeController::class, '__invoke']);
